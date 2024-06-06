9b3fc4c3583e655beec8339cfa391708a16d87f0
Hayden Riddiford <hayden@terrakaffe.com>
2024-06-06 09:45:55 -0700

    - Updated build.zig to automatically find library folders and the arm-none-eabi-gcc executable - Removed verbose hard coding since that can just be set on the command line

--- build.zig
+++ build.zig
@@ -9,13 +9,11 @@ pub fn build(b: *std.Build) void {
         .cpu_features_add = std.Target.arm.featureSet(&[_]std.Target.arm.Feature{std.Target.arm.Feature.fp_armv8d16sp}),
     });
 
-    const arm_gcc_version = "10.3.1";
-    const project_name = "blinky";
+    const executable_name = "blinky";
 
-    // b.verbose = true;
     const optimize = b.standardOptimizeOption(.{});
     const blinky_exe = b.addExecutable(.{
-        .name = project_name ++ ".elf",
+        .name = executable_name ++ ".elf",
         .target = target,
         .optimize = optimize,
         .link_libc = false,
@@ -23,23 +21,45 @@ pub fn build(b: *std.Build) void {
         .single_threaded = true,
     });
 
-    // Manually including libraries bundled with arm-none-eabi-gcc
-    const arm_gcc_path = b.option([]const u8, "armgcc", "Path to arm-none-eabi-gcc compiler") orelse unreachable;
-    blinky_exe.addLibraryPath(.{ .path = b.fmt("{s}/arm-none-eabi/lib/thumb/v7e-m+fp/hard", .{arm_gcc_path}) });
-    blinky_exe.addLibraryPath(.{ .path = b.fmt("{s}/lib/gcc/arm-none-eabi/" ++ arm_gcc_version ++ "/thumb/v7e-m+fp/hard", .{arm_gcc_path}) });
-    blinky_exe.addSystemIncludePath(.{ .path = b.fmt("{s}/arm-none-eabi/include", .{arm_gcc_path}) });
+    // User Options
+
+    // Try to find arm-none-eabi-gcc program at a user specified path, or PATH variable if none provided
+    const arm_gcc_pgm = if (b.option([]const u8, "armgcc", "Path to arm-none-eabi-gcc compiler")) |arm_gcc_path|
+        b.findProgram(&.{"arm-none-eabi-gcc"}, &.{arm_gcc_path}) catch {
+            std.log.err("Couldn't find arm-none-eabi-gcc at provided path: {s}\n", .{arm_gcc_path});
+            unreachable;
+        }
+    else
+        b.findProgram(&.{"arm-none-eabi-gcc"}, &.{}) catch {
+            std.log.err("Couldn't find arm-none-eabi-gcc in PATH, try manually providing the path to this executable with -Darmgcc=[path]\n", .{});
+            unreachable;
+        };
+
+    // Allow user to enable float formatting in newlib (printf, sprintf, ...)
+    if (b.option(bool, "NEWLIB_PRINTF_FLOAT", "Force newlib to include float support for printf()")) |_| {
+        blinky_exe.forceUndefinedSymbol("_printf_float"); // GCC equivalent : "-u _printf_float"
+    }
+
+    //  Use gcc-arm-none-eabi to figure out where library paths are
+    const gcc_arm_sysroot_path = std.mem.trim(u8, b.run(&.{ arm_gcc_pgm, "-print-sysroot" }), "\r\n");
+    const gcc_arm_multidir_relative_path = std.mem.trim(u8, b.run(&.{ arm_gcc_pgm, "-mcpu=cortex-m7", "-mfpu=fpv5-sp-d16", "-mfloat-abi=hard", "-print-multi-directory" }), "\r\n");
+    const gcc_arm_version = std.mem.trim(u8, b.run(&.{ arm_gcc_pgm, "-dumpversion" }), "\r\n");
+    const gcc_arm_lib_path1 = b.fmt("{s}/../lib/gcc/arm-none-eabi/{s}/{s}", .{ gcc_arm_sysroot_path, gcc_arm_version, gcc_arm_multidir_relative_path });
+    const gcc_arm_lib_path2 = b.fmt("{s}/lib/{s}", .{ gcc_arm_sysroot_path, gcc_arm_multidir_relative_path });
+
+    // Manually add "nano" variant newlib C standard lib from arm-none-eabi-gcc library folders
+    blinky_exe.addLibraryPath(.{ .path = gcc_arm_lib_path1 });
+    blinky_exe.addLibraryPath(.{ .path = gcc_arm_lib_path2 });
+    blinky_exe.addSystemIncludePath(.{ .path = b.fmt("{s}/include", .{gcc_arm_sysroot_path}) });
     blinky_exe.linkSystemLibrary("c_nano");
     blinky_exe.linkSystemLibrary("m");
 
-    // Allow float formating (printf, sprintf, ...)
-    //blinky_exe.forceUndefinedSymbol("_printf_float"); // GCC equivalent : "-u _printf_float"
-
     // Manually include C runtime objects bundled with arm-none-eabi-gcc
-    blinky_exe.addObjectFile(.{ .path = b.fmt("{s}/arm-none-eabi/lib/thumb/v7e-m+fp/hard/crt0.o", .{arm_gcc_path}) });
-    blinky_exe.addObjectFile(.{ .path = b.fmt("{s}/lib/gcc/arm-none-eabi/" ++ arm_gcc_version ++ "/thumb/v7e-m+fp/hard/crti.o", .{arm_gcc_path}) });
-    blinky_exe.addObjectFile(.{ .path = b.fmt("{s}/lib/gcc/arm-none-eabi/" ++ arm_gcc_version ++ "/thumb/v7e-m+fp/hard/crtbegin.o", .{arm_gcc_path}) });
-    blinky_exe.addObjectFile(.{ .path = b.fmt("{s}/lib/gcc/arm-none-eabi/" ++ arm_gcc_version ++ "/thumb/v7e-m+fp/hard/crtend.o", .{arm_gcc_path}) });
-    blinky_exe.addObjectFile(.{ .path = b.fmt("{s}/lib/gcc/arm-none-eabi/" ++ arm_gcc_version ++ "/thumb/v7e-m+fp/hard/crtn.o", .{arm_gcc_path}) });
+    blinky_exe.addObjectFile(.{ .path = b.fmt("{s}/crt0.o", .{gcc_arm_lib_path2}) });
+    blinky_exe.addObjectFile(.{ .path = b.fmt("{s}/crti.o", .{gcc_arm_lib_path1}) });
+    blinky_exe.addObjectFile(.{ .path = b.fmt("{s}/crtbegin.o", .{gcc_arm_lib_path1}) });
+    blinky_exe.addObjectFile(.{ .path = b.fmt("{s}/crtend.o", .{gcc_arm_lib_path1}) });
+    blinky_exe.addObjectFile(.{ .path = b.fmt("{s}/crtn.o", .{gcc_arm_lib_path1}) });
 
     // Normal Include Paths
     blinky_exe.addIncludePath(b.path("Core/Inc"));
@@ -85,22 +105,21 @@ pub fn build(b: *std.Build) void {
     blinky_exe.link_function_sections = true;
     blinky_exe.setLinkerScriptPath(.{ .path = "./STM32F750N8Hx_FLASH.ld" });
 
-    // Copy the bin out of the elf
+    // Produce .bin file from .elf
     const bin = b.addObjCopy(blinky_exe.getEmittedBin(), .{
         .format = .bin,
     });
     bin.step.dependOn(&blinky_exe.step);
-    const copy_bin = b.addInstallBinFile(bin.getOutput(), project_name ++ ".bin");
+    const copy_bin = b.addInstallBinFile(bin.getOutput(), executable_name ++ ".bin");
     b.default_step.dependOn(&copy_bin.step);
 
-    // Copy the bin out of the elf
+    // Produce .hex file from .elf
     const hex = b.addObjCopy(blinky_exe.getEmittedBin(), .{
         .format = .hex,
     });
     hex.step.dependOn(&blinky_exe.step);
-    const copy_hex = b.addInstallBinFile(hex.getOutput(), project_name ++ ".hex");
+    const copy_hex = b.addInstallBinFile(hex.getOutput(), executable_name ++ ".hex");
     b.default_step.dependOn(&copy_hex.step);
 
     b.installArtifact(blinky_exe);
-    // blinky_exe.setVerboseLink(true);
 }
